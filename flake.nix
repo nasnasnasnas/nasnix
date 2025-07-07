f0c56b4e295a8180f0da763038e2acaa2f9e69d3
nea <git@nea.dev>
2025-07-06 20:48:57 -0400

    haumea init

--- flake.nix
+++ flake.nix
@@ -1,41 +1,56 @@
 {
-  description = "A very basic flake";
+  description = "NAS Nix";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    snowfall-lib = {
-      url = "github:snowfallorg/lib";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixos-generators.url = "github:nix-community/nixos-generators";
-    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
+    haumea = {
+      url = "github:nix-community/haumea/v0.2.2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs:
-    inputs.snowfall-lib.mkFlake {
-      inherit inputs;
+  outputs = inputs@{ nixpkgs, home-manager, haumea, ... }: {
+    lib = haumea.lib.load {
       src = ./.;
-
-      snowfall = {
-        namespace = "nea-and-saige";
-
-        meta = {
-          name = "nas-flake";
-
-          title = "NAS Flake";
-        };
-      };
-
-      outputs-builder = channels: {
-        formatter = channels.nixpkgs.alejandra;
+      inputs = {
+        inherit (nixpkgs) lib;
+        inherit (home-manager) lib;
+        inherit (haumea) lib;
       };
     };
+
+    nixosConfigurations = {
+      # TODO please change the hostname to your own
+      saige-macbook-nixos = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+
+        specialArgs = { inherit inputs; nixosModules = [ ./modules/nixos ]; };
+        modules = [
+          ./hosts/saige-macbook-nixos.nix
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # TODO also import saige@saige-macbook-nixos if it exists
+            home-manager.users.saige = import ./users/saige.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
+    };
+  };
 }
